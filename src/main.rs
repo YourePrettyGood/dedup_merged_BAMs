b8cf37b64d5e1efc63e4792bf22b73f6c3b60f1c
Patrick F. Reilly <pegasusflightresources@gmail.com>
2022-05-10 14:10:42 -0400

    Second attempt, still untested, but changed out the fixed-size VecDeque buffer for a variable-size HashSet that gets cleared whenever the reference base under consideration changes. Matching start position is a precondition for duplication, so a variable-size buffer will catch everything and has better complexity for search and insertion (though we will be in the small n region most of the time, so the constants may matter).

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use anyhow::{Context, Result};
 use clap::Parser;
 use rust_htslib::{bam, bam::Read};
-use std::collections::VecDeque;
-//use fixed_vec_deque::FixedVecDeque;
+use std::collections::HashSet;
 
 /// A tool to remove artificial duplicate alignment records from a BAM caused by merging BAMs split by region with reads overlapping the gap between regions.
 #[derive(Parser, Debug)]
@@ -14,8 +13,9 @@ struct CLIargs {
    /// Output BAM path
    #[clap(short, long, parse(try_from_str))]
    output: String,
-   /// Size of alignment buffer to compare against for duplicate detection
-   #[clap(short, long, default_value_t = 20usize, parse(try_from_str))]
+   /// Preallocated size of alignment buffer to compare against for duplicate detection
+   /// Try setting it to the per-base depth, though that may be overkill
+   #[clap(short, long, default_value_t = 50usize, parse(try_from_str))]
    buffer_size: usize,
    /// Number of threads for BAM reading and writing
    #[clap(short, long, default_value_t = 1usize, parse(try_from_str))]
@@ -37,6 +37,7 @@ fn main() -> Result<()> {
       //Match a path:
       path    => bam::Reader::from_path(path).context(format!("Could not read input BAM: {}", args.input))?
    };
+
    //Set the number of reading threads:
    input_bam.set_threads(args.threads)
       .context(format!("Failed to set number of BAM reading threads to {}", args.threads))?;
@@ -47,32 +48,43 @@ fn main() -> Result<()> {
    //Open the output BAM:
    let mut output_bam = bam::Writer::from_path(&args.output, &bam_header, bam::Format::Bam)
       .context(format!("Could not open output BAM: {}", args.output))?;
+
    //Set the number of writing threads:
    output_bam.set_threads(args.threads)
       .context(format!("Failed to set number of BAM writing threads to {}", args.threads))?;
+
    //Set the compression level of the output BAM:
    let bam_compression_level = bam::CompressionLevel::Level(args.compression_level);
    output_bam.set_compression_level(bam_compression_level)
       .context(format!("Failed to set compression level of output BAM to {}", args.compression_level))?;
 
-   //Create the alignment buffer vector, though we really only store the QNAME, RNAME, POS, and FLAGS in a tuple:
-//   let mut aln_buffer = FixedVecDeque::<[(&[u8], i32, i64, u16); buffer_size]>::new();
-   let mut aln_buffer: VecDeque<(String, i32, i64, u16)> = VecDeque::with_capacity(args.buffer_size);
+   //Create the alignment buffer hash set, though we really only store the QNAME, RNAME, POS, and FLAGS in a tuple:
+   let mut aln_buffer: HashSet<(String, i32, i64, u16)> = HashSet::with_capacity(args.buffer_size);
+
+   //Create a state tuple of RNAME and POS so that we clear the set upon every position change:
+   //Note that the initial state doesn't actually matter.
+   let mut which_base: (i32, i64) = (0i32, 0i64);
 
    //Iterate through batches of alignment records:
    for read_result in input_bam.rc_records() {
       //Check if the alignment is in the buffer:
       let read = read_result.context("Unable to read alignment record")?;
+
+      //Clear out the hash set if we've moved to the next reference base, since duplicates must have matching RNAME and POS:
+      if which_base != (read.tid(), read.pos()) {
+         aln_buffer.clear();
+         which_base = (read.tid(), read.pos());
+      }
+
       //Annoyingly, rust_htslib::bam::Record::qname() returns a &[u8], so we need to convert to a String:
       let read_qname = std::str::from_utf8(read.qname()).context("Unable to convert read name to UTF8 string, something's weird here.")?;
+
       let aln_key = (read_qname.to_owned(), read.tid(), read.pos(), read.flags());
+
       if !aln_buffer.contains(&aln_key) {
-         //If not, add it to the buffer (cycling out an element if over capacity), and output the alignment:
-//         *aln_buffer.push_back() = aln_key;
-         if aln_buffer.len() == args.buffer_size {
-            _ = aln_buffer.pop_front();
-         }
-         aln_buffer.push_back(aln_key);
+         //If not, add it to the buffer, and output the alignment:
+         aln_buffer.insert(aln_key);
+
          output_bam.write(&read)
             .context("Failed to write alignment to output BAM")?;
       }
